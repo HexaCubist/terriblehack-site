cc095a95276ef0920a0d9adfc459a1113684cdf7
Zac M-W <contact@zac.nz>
2025-04-01 01:41:54 +0100

    Fix responsive feature list

--- src/lib/components/featureList.svelte
+++ src/lib/components/featureList.svelte
@@ -10,15 +10,15 @@
 </script>
 
 {#each items as item}
-	<div class="mx-auto my-4 flex w-auto flex-row items-center justify-center gap-4 sm:my-8 sm:gap-8">
-		<div class="">
-			<div
-				class="bg-base-content text-base-100 flex h-full w-10 items-center justify-center rounded-full sm:size-16"
-			>
-				<Icon icon={item.icon} class="size-6 sm:size-8" />
-			</div>
+	<div
+		class="relative mx-auto my-4 flex w-auto flex-row justify-center gap-4 sm:my-8 sm:items-center sm:gap-8"
+	>
+		<div
+			class="bg-base-content text-base-100 absolute left-0 flex h-full items-start justify-center rounded-xl p-2 sm:relative sm:size-16 sm:items-center sm:rounded-full"
+		>
+			<Icon icon={item.icon} class="size-4 sm:size-8" />
 		</div>
-		<div class="max-w-prose grow">
+		<div class="ml-11 max-w-prose grow sm:ml-0">
 			<h3 class="font-bold">{item.title}</h3>
 			<p class="text-sm sm:text-base">{item.description}</p>
 		</div>
